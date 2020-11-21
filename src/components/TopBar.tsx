4503798972b0fcb354c51c242de789cda2fca5ac
Felix Mohr <felix-mohr@hotmail.de>
2020-11-21 16:05:45 +0100

    theme

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,15 @@
-import React, { FC, ReactElement } from 'react'
+import React, { ReactElement, useContext } from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+import SunIcon from '@material-ui/icons/WbSunnyOutlined'
+import MoonIcon from '@material-ui/icons/Brightness2Outlined'
+import CodeIcon from '@material-ui/icons/Code'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import AppBar from '@material-ui/core/AppBar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import { makeStyles } from '@material-ui/core/styles'
+import { useTheme } from '@emotion/react'
+import { ToggleThemeContext } from '../theme'
 
 const useStyles = makeStyles({
   show: {
@@ -17,20 +20,39 @@ const useStyles = makeStyles({
     transform: 'translateY(-110%)',
     transition: 'transform .5s',
   },
+  toolbar: {
+    paddingLeft: '10%',
+  },
+  toolbarContent: {
+    paddingLeft: '10%',
+  },
+  toolbarRight: {
+    right: 0,
+    position: 'absolute',
+    paddingRight: '10%',
+  },
 })
 
 export const TopBar = (): ReactElement => {
   const trigger = useScrollTrigger()
   const classes = useStyles()
+  const { toggleTheme, isDark } = useContext(ToggleThemeContext)
 
   return (
     <AppBar className={trigger ? classes.hide : classes.show} position="sticky">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6">News</Typography>
-        <Button color="inherit">Login</Button>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6">DevFullStack</Typography>
+        <div className={classes.toolbarContent}>
+          <Button color="inherit">
+            <CodeIcon />
+            &nbsp;Topics
+          </Button>
+        </div>
+        <div className={classes.toolbarRight}>
+          <Button color="inherit" onClick={toggleTheme}>
+            {isDark ? <SunIcon /> : <MoonIcon />}
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   )
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { lightTheme, darkTheme } from './theme'
 
 interface ThemeProviderProps {
   children: React.ReactNode
 }
 
+export const ToggleThemeContext = React.createContext({
+  toggleTheme: () => {
+    console.log()
+  },
+  isDark: false,
+})
+
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }: ThemeProviderProps) => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const appliedTheme = prefersDarkMode ? darkTheme : lightTheme
+  const [selectedTheme, setSelectedTheme] = useState<string>('')
+  const [appliedTheme, setAppliedTheme] = useState(darkTheme)
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme')
+    if (theme && theme === 'lightTheme') {
+      setSelectedTheme('lightTheme')
+      setAppliedTheme(lightTheme)
+    } else if (theme && theme === 'darkTheme') {
+      setSelectedTheme('darkTheme')
+      setAppliedTheme(darkTheme)
+    }
+  }, [])
+
+  const toggleTheme = useCallback(() => {
+    if (!selectedTheme || selectedTheme === 'darkTheme') {
+      setSelectedTheme('lightTheme')
+      setAppliedTheme(lightTheme)
+      localStorage.setItem('theme', 'lightTheme')
+    } else {
+      setSelectedTheme('darkTheme')
+      localStorage.setItem('theme', 'darkTheme')
+      setAppliedTheme(darkTheme)
+    }
+  }, [selectedTheme, setSelectedTheme])
 
   return (
-    <MuiThemeProvider theme={appliedTheme}>
-      <CssBaseline />
-      {children}
-    </MuiThemeProvider>
+    <ToggleThemeContext.Provider value={{ toggleTheme, isDark: !(selectedTheme === 'lightTheme') }}>
+      <MuiThemeProvider theme={appliedTheme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </ToggleThemeContext.Provider>
   )
 }
 
