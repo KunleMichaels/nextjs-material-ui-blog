89798509800a8f70cff789ab944bb110b4669f40
Felix Mohr <felix-mohr@hotmail.de>
2020-11-25 19:21:17 +0100

    test commit to dev

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const globby = require('globby')
+
+async function generateSiteMap() {
+  const pages = await globby(['pages/**/*.tsx', '!pages/_*.tsx', '!pages/**/[**].tsx', 'posts/*.md'])
+  console.log('pages are', pages)
+
+  const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages
+            .map((page) => {
+              const path = page.replace('pages', '').replace('.js', '').replace('.md', '')
+              const route = path === '/index' ? '' : path
+              return `
+                      <url>
+                          <loc>${`https://devx.sh/${route}`}</loc>
+                      </url>
+                  `
+            })
+            .join('')}
+      </urlset>
+  `
+
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+}
+
+generateSiteMap()
