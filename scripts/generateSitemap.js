ed808eb66332228a5e4ebf58f917ef3658f1bf77
Felix Mohr <felix-mohr@hotmail.de>
2020-11-25 19:57:10 +0100

    building sitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,26 +1,41 @@
 const fs = require('fs')
 const globby = require('globby')
+const matter = require('gray-matter')
 
 async function generateSiteMap() {
-  const pages = await globby(['pages/**/*.tsx', '!pages/_*.tsx', '!pages/**/[**].tsx', 'posts/*.md'])
-  console.log('pages are', pages)
+  const patterns = ['pages/**/*.tsx', '!pages/_*.tsx', `!pages/**/\\[*\\].tsx`, '!pages/404.tsx', 'posts/*.md']
+  let pages = await globby(patterns)
 
-  const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          ${pages
-            .map((page) => {
-              const path = page.replace('pages', '').replace('.js', '').replace('.md', '')
-              const route = path === '/index' ? '' : path
-              return `
-                      <url>
-                          <loc>${`https://devx.sh/${route}`}</loc>
-                      </url>
-                  `
-            })
-            .join('')}
-      </urlset>
-  `
+  const fileNames = fs.readdirSync('posts')
+  const topics = Array.from(
+    new Set(
+      fileNames.reduce((prev, fn) => {
+        const fileContents = fs.readFileSync('posts/' + fn, 'utf8')
+        const matterResult = matter(fileContents)
+        const topics = matterResult.data.topics.split(',')
+        return [...prev, ...topics]
+      }, []),
+    ),
+  )
+
+  pages = [...pages, ...topics.map((t) => `/topics/${t}`)]
+
+  const sitemap =
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
+    `${pages
+      .map((page) => {
+        const path = page
+          .replace('pages', '')
+          .replace('.tsx', '')
+          .replace('.md', '')
+          .replace('posts/', '/blog/')
+          .replace('/index', '')
+        const route = path === '/index' ? '' : path
+        return `
+                      <url><loc>${`https://devx.sh${route}`}</loc></url>\n`.trimStart()
+      })
+      .join('')}</urlset>`.trim()
 
   fs.writeFileSync('public/sitemap.xml', sitemap)
 }
